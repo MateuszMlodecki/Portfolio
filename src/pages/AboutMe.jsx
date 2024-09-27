c4deb606e2712400c8a32b4c1e44e2285cecb2b5
Mateusz Mlodecki <mateuszmlodecki@icloud.com>
2024-09-27 21:40:49 +0200

    added some bugs

--- src/pages/AboutMe.jsx
+++ src/pages/AboutMe.jsx
@@ -1,5 +1,10 @@
 import React from "react";
+import { Typography } from "@mui/material";
 
 export const AboutMe = () => {
-  return <div>AboutMe</div>;
+  return <div className="bg-[#DDDDDD] min-h-screen p-8">
+
+    <Typography>Cześć, jestem Mateusz, pracownik magazynu z pięcioletnim doświadczeniem, aspirujący do zostania programistą. Moja pasja do rozwiązywania problemów i programowania narodziła się dzięki samodzielnie przerabianym kursom online. Obecnie, po pracy, poświęcam się nauce HTML5, CSS i JavaScript, wierząc, że determinacja i dyscyplina doprowadzą mnie do udanej kariery w IT.</Typography>
+  </div>;
 };
+
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Avatar, Box, Typography } from "@mui/material";
+import profilepic from "../images/avatar.png"
 
 export const Home = () => {
   return (
@@ -20,27 +21,24 @@ export const Home = () => {
               fontSize: 24,
             }}
           >
-            Cześć, jestem Mateusz, pracownik magazynu z pięcioletnim
-            doświadczeniem, aspirujący do zostania programistą. Moja pasja do
-            rozwiązywania problemów i programowania narodziła się dzięki
-            samodzielnie przerabianym kursom online. Obecnie, po pracy,
-            poświęcam się nauce HTML5, CSS i JavaScript, wierząc, że
-            determinacja i dyscyplina doprowadzą mnie do udanej kariery w IT.
+           Cześć! Mam na imię Mateusz, jestem początkującym website 
+           developerem z pasją do tworzenia nowoczesnych i responsywnych stron internetowych. 
+           Specjalizuję się w technologiach <b>Frontend</b>, takich jak <b>HTML, CSS, JavaScript oraz React.js, </b> 
+           ale cały czas staram się poszerzać moją wiedzę.
+            Każdy nowy projekt traktuję jako okazję do nauki i rozwoju, a to portfolio jest zbiorem moich dotychczasowych osiągnięć.
+            Zapraszam do obejrzenia mojej pracy!
           </Typography>
         </div>
         {/* Avatar */}
         <div className="slideInFromRight mt-8 md:mt-0 md:w-1/2 flex justify-center pb-2">
           <Avatar
             alt="Profile picture"
-            src="./Photo2.png"
+            src={profilepic}
             sx={{ width: 300, height: 300, backgroundColor: "#f2f2f2f2" }}
           />
         </div>
       </div>
-      {/* Fotosection */}
-      <div className="p-10">
-        <img src="./homepage.jpg" alt="" style={{ width: 993 }} />
-      </div>
+      <div className="pb-14"></div>
     </div>
   );
 };
